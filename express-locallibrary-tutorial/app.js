7985fad6192d10a36855f8cf4d6048e6d405026e
Damien Rousselot <drousselot2007@gmail.com>
2021-02-28 16:07:41 +0100

    preparation pour heroku

--- express-locallibrary-tutorial/app.js
+++ express-locallibrary-tutorial/app.js
@@ -16,7 +16,8 @@ app.use(helmet());
 
 //Set up mongoose connection
 var mongoose = require('mongoose');
-var mongoDB = 'mongodb+srv://MyBase:g1id2gni@cluster0.858qw.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
+var dev_db_url = 'mongodb+srv://MyBase:g1id2gni@cluster0.858qw.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
+var mongoDB = process.env.MONGODB_URI || dev_db_url;
 mongoose.connect(mongoDB, { useNewUrlParser: true , useUnifiedTopology: true});
 var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'MongoDB connection error:'));
